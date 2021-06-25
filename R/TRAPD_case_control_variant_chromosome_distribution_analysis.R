3780acc7562695bb6404ce31f98f3e98a7d85d32
YucanChen <63009092+YucanChen@users.noreply.github.com>
2021-06-25 22:43:24 +0800

    Update TRAPD_case_control_variant_chromosome_distribution_analysis.R

--- R/TRAPD_case_control_variant_chromosome_distribution_analysis.R
+++ R/TRAPD_case_control_variant_chromosome_distribution_analysis.R
@@ -49,3 +49,83 @@ df_chr_earlyAD_test <- as.data.frame(t(df_chr_earlyAD_test));df_chr_earlyAD_test
 write.csv(df_chr_dementia_test,"df_chr_dementia_test.csv")
 write.csv(df_chr_AD_test,"df_chr_AD_test.csv")
 write.csv(df_chr_earlyAD_test,"df_chr_earlyAD_test.csv")
+
+# drawing forest plot
+library("forestplot")
+library(lattice)
+## dementia
+OR_interval_df_dementia <- 
+  structure(list(
+    mean  = c(NA,result_dementia$odds_ratio), 
+    lower = c(NA,result_dementia$conf.int_lower),
+    upper = c(NA,result_dementia$conf.int_upper)),
+    .Names = c("mean", "lower", "upper"), 
+    row.names = c(NA, -23L),
+    class = "data.frame")
+tabletext_dementia<-cbind(
+  c("Chromosome",paste("chr",result_dementia$chr,sep = "")),
+  c("OR",sprintf("%0.2f",result_dementia$odds_ratio)),
+  c("CI",paste("(",paste(sprintf("%0.2f",result_dementia$conf.int_lower),sprintf("%0.2f",result_dementia$conf.int_upper),sep = "-"),")",sep = "")),
+  c("p.value",sprintf("%0.3f",result_dementia$p_value)),
+  c("p.adjust",sprintf("%0.3f",result_dementia$adj_pvalue)))
+trellis.device(device = "windows", height = 25, width = 40, color = TRUE)
+forestplot(tabletext_dementia,
+           graph.pos = 2,
+           hrzl_lines = list("2" = gpar(lty=2),
+                            "24" = gpar(lwd=1, lty=2, col = "#000044")),
+           OR_interval_df_dementia,new_page = TRUE,
+           clip=c(0.1,2.5), 
+           xlog=TRUE,
+           col=fpColors(box="royalblue",line="darkblue", hrz_lines = "#444444")
+          )
+## AD
+OR_interval_df_AD <- 
+  structure(list(
+    mean  = c(NA,result_AD$odds_ratio), 
+    lower = c(NA,result_AD$conf.int_lower),
+    upper = c(NA,result_AD$conf.int_upper)),
+    .Names = c("mean", "lower", "upper"), 
+    row.names = c(NA, -23L),
+    class = "data.frame")
+tabletext_AD<-cbind(
+  c("Chromosome",paste("chr",result_AD$chr,sep = "")),
+  c("OR",sprintf("%0.2f",result_AD$odds_ratio)),
+  c("CI",paste("(",paste(sprintf("%0.2f",result_AD$conf.int_lower),sprintf("%0.2f",result_AD$conf.int_upper),sep = "-"),")",sep = "")),
+  c("p.value",sprintf("%0.3f",result_AD$p_value)),
+  c("p.adjust",sprintf("%0.3f",result_AD$adj_pvalue)))
+trellis.device(device = "windows", height = 25, width = 40, color = TRUE)
+forestplot(tabletext_AD,
+           graph.pos = 2,
+           hrzl_lines = list("2" = gpar(lty=2),
+                             "24" = gpar(lwd=1, lty=2, col = "#000044")),
+           OR_interval_df_AD,new_page = TRUE,
+           clip=c(0.1,2.5), 
+           xlog=TRUE,
+           col=fpColors(box="royalblue",line="darkblue", hrz_lines = "#444444")
+)
+## AD early
+OR_interval_df_earlyAD <- 
+  structure(list(
+    mean  = c(NA,result_earlyAD$odds_ratio), 
+    lower = c(NA,result_earlyAD$conf.int_lower),
+    upper = c(NA,result_earlyAD$conf.int_upper)),
+    .Names = c("mean", "lower", "upper"), 
+    row.names = c(NA, -23L),
+    class = "data.frame")
+tabletext_earlyAD<-cbind(
+  c("Chromosome",paste("chr",result_earlyAD$chr,sep = "")),
+  c("OR",sprintf("%0.2f",result_earlyAD$odds_ratio)),
+  c("CI",paste("(",paste(sprintf("%0.2f",result_earlyAD$conf.int_lower),sprintf("%0.2f",result_earlyAD$conf.int_upper),sep = "-"),")",sep = "")),
+  c("p.value",sprintf("%0.3f",result_earlyAD$p_value)),
+  c("p.adjust",sprintf("%0.3f",result_earlyAD$adj_pvalue)))
+trellis.device(device = "windows", height = 25, width = 40, color = TRUE)
+forestplot(tabletext_earlyAD,
+           graph.pos = 2,
+           hrzl_lines = list("2" = gpar(lty=2),
+                             #"11" = gpar(lwd=1, columns=c(1:3,5), col = "#000044"),
+                             "24" = gpar(lwd=1, lty=2, col = "#000044")),
+           OR_interval_df_earlyAD,new_page = TRUE,
+           clip=c(0.1,2.5), 
+           xlog=TRUE,
+           col=fpColors(box="royalblue",line="darkblue", hrz_lines = "#444444")
+)
